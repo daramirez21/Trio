a4bf1726ef997ac11a6a760e02bf5c613bf9e43d
daramirez21 <daramirez@mail.csuchico.edu>
2019-11-13 16:44:20 -0800

    Rearange

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 // <-- ADD YOUR FUNCTION PROTOTYPE HERE
 
+
+void greatestToLeast(int &, int &, int &);
+
+
+
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -11,9 +16,9 @@ int main()
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
-
   // <-- ADD YOUR FUNCTION CALL HERE
 
+    greatestToLeast(red, green, blue);
 
   //DO NOT CHANGE WITHIN THIS AREA...
   cout<<"Rearranged....\n";
@@ -23,3 +28,28 @@ int main()
 }
 
 // <-- ADD YOUR FUNCTION DEFINITON HERE
+
+void greatestToLeast(int &a, int &b, int &c)
+{
+   int temp = 0;
+
+    if( a < b) 
+    {
+     temp = a;
+     a = b;
+     b = temp;
+    }
+    if(a < c)
+    {
+     temp = a;
+     a = c;
+     c = temp;
+    }
+    if( b < c)
+    {
+     temp = b;
+     b = c;
+     c = temp;
+    }
+}
+
